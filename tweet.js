ff73f8265ff0fc8d6de7966e957ff4b698f3f123
Scott Duncombe <srduncombe@gmail.com>
2014-09-23 03:39:43 -0700

    Add kue config to tweets

--- tweet.js
+++ tweet.js
@@ -1,6 +1,16 @@
 var kue = require('kue')
   , jobs = kue.createQueue();
 
+kue.redis.createClient = function() {
+    var redisUrl = url.parse(process.env.REDISTOGO_URL)
+    var client = redis.createClient(redisUrl.port, redisUrl.hostname)
+
+    if (redisUrl.auth) {
+        client.auth(redisUrl.auth.split(":")[1]);
+    }
+
+    return client;
+}
 
 var Twitter = require('node-tweet-stream')
   , t = new Twitter({
