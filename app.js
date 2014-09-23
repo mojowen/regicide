b1334aa5b134542553dab0553d9cd55e856834b9
Scott Duncombe <srduncombe@gmail.com>
2014-09-23 03:38:37 -0700

    update kue app to work on heroku

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-var kue = require('kue');
-kue.app.listen(3000);
+var kue = require('kue')
+
+kue.redis.createClient = function() {
+    var redisUrl = url.parse(process.env.REDISTOGO_URL)
+    var client = redis.createClient(redisUrl.port, redisUrl.hostname)
+
+    if (redisUrl.auth) {
+        client.auth(redisUrl.auth.split(":")[1]);
+    }
+    return client;
+}
+
+kue.app.listen(process.env.PORT || 3000)
