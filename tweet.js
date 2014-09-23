bf6d89c5230772002ef6e559a9c2a4e1fc087ebc
Scott Duncombe <srduncombe@gmail.com>
2014-09-23 03:14:20 -0700

    getting kue ready

---
+++ tweet.js
@@ -0,0 +1,23 @@
+var kue = require('kue')
+  , jobs = kue.createQueue();
+
+
+var Twitter = require('node-tweet-stream')
+  , t = new Twitter({
+    consumer_key: process.env.TWITTER,
+    consumer_secret: process.env.TWITTER_SECRET,
+    token: process.env.TWITTER_OATH,
+    token_secret: process.env.TWITTER_OATH_SECRET
+  })
+
+t.on('tweet', function (tweet) {
+    var job = jobs.create('tweet', tweet).save( function(err){
+       if( !err ) console.log( job.id );
+    });
+})
+
+t.on('error', function (err) {
+  console.log('Oh no')
+})
+
+t.track('nodejs')
